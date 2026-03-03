14e29dbebe247cf4657ecf6ed918fb7b5ae27a20
rahulvish100 <rahul@gmail.com>
2026-03-03 15:55:55 +0530

    Initial commit

---
+++ create-github-repo.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Creating GitHub repository...
+echo.
+echo You need to:
+echo 1. Go to https://github.com/new
+echo 2. Repository name: portfolio
+echo 3. Select Public
+echo 4. Don't add README
+echo 5. Click Create repository
+echo.
+echo After creating, run: git push -u origin master
+echo.
+pause
